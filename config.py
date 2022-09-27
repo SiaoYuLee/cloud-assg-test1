24a637b956ceb556dc348a6e49e54cb9b2e332d7
SiaoYuLee <99385170+SiaoYuLee@users.noreply.github.com>
2022-09-27 23:26:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee1.cceox3i7xlby.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee1"
+custombucket = "leesiaoyu-employee"
+customregion = "us-east-1"
